d63c7e06f84c9b0baff9d467a52b004cd251b211
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-08 09:25:28 -0700

    update home skills component

--- src/components/Home/Skills.jsx
+++ src/components/Home/Skills.jsx
@@ -4,41 +4,44 @@ import './Skills.css'
 const Skills = () => {
   return ( 
     <div className='skills-container'>
-      <h1>My Skills</h1>
+      <h1>Top Skills</h1>
       <div className='skill-card-container'>
         <div className='skill-card'>
+          <img src="./src/assets/tech-icon.svg" alt="tech icon"/>
           <h3>Technologies</h3>
           <ul>
-            <p>HTML/CSS</p>
-            <p>JavaScript</p>
-            <p>React</p>
-            <p>Python</p>
-            <p>MongoDB</p>
-            <p>Mongoose</p>
+            <li>HTML/CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Python</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
           </ul>
         </div>
         <div className='skill-card'>
+          <img src="./src/assets/framework-icon.svg" alt="tech icon"/>
         <h3>Frameworks & Services</h3>
           <ul>
-            <p>Google OAuth</p>
-            <p>RESTful Routing</p>
-            <p>JSON</p>
-            <p>JWT Authentication</p>
-            <p>API</p>
-            <p>Node.js</p>
-            <p>Express.js</p>
-            <p>Django</p>
-            <p>Github</p>
+            <li>Github</li>
+            <li>Google OAuth</li>
+            <li>JWT Authentication</li>
+            <li>JSON</li>
+            {/* <li>API</li> */}
+            <li>Node.js/Express.js</li>
+            {/* <li>Express.js</li> */}
+            <li>Django</li>
           </ul>
         </div>
         <div className='skill-card'>
+          <img src="./src/assets/method-icon.svg" alt="tech icon"/>
         <h3>Methodologies</h3>
           <ul>
-            <p>Agile development methodology</p>
-            <p>Scrum</p>
-            <p>A/B Testing</p>
-            <p>QA Testing</p>
-            <p>Debugging</p>
+            <li>Agile development</li>
+            <li>RESTful Routing</li>
+            <li>Scrum</li>
+            <li>A/B Testing</li>
+            <li>QA Testing</li>
+            <li>Debugging</li>
           </ul>
         </div>
       </div>
