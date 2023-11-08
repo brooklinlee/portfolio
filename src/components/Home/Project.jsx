3663845c12efa85cd75682806eb020c6558b36b4
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-08 14:54:59 -0700

    paginate projects on home page

--- src/components/Home/Project.jsx
+++ src/components/Home/Project.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // components
 import ProjectCard from './ProjectCard'
 
@@ -5,70 +7,97 @@ import ProjectCard from './ProjectCard'
 import './Project.css'
 
 const ProjectList = () => {
-  const cards = [
+  const items = [  
     {
       name: 'WellFit - Workout Tracker',
       tech: 'Python / Django / Docker',
-      // description: 'Workout Tracker',
       image: ('./src/assets/wellfit-pc.svg'),
     },
     {
       name: 'Wandr - Travel Themed Social Media',
       tech: 'Full-Stack / Group Git Workflow / React / MongoDB & Mongoose',
-      // description: 'Travel themed soci',
-      image:('./src/assets/wellfit-pc.svg'),
+      image:('./src/assets/wandr-pc.svg'),
     },
     {
       name: 'The Yum Yum Hub - Recipe Collector',
       tech: 'Python / Django / Docker',
-      // description: '',
-      image: ('./src/assets/wellfit-pc.svg'),
+      image: ('./src/assets/yyh-pc.svg'),
     },
     {
       name: 'Word Whimsy - Online Journaling',
       tech: 'JavaScript / Express.js / MongoDB & Mongoose',
-      // description: '',
-      image: ('./src/assets/wellfit-pc.svg'),
+      image: ('./src/assets/word-whimsy-pc.svg'),
     },
     {
       name: 'The Silly Goose Grape-Venture',
       tech: 'HTML & CSS / JavaScript / Game Logic',
-      // description: '',
-      image: ('./src/assets/wellfit-pc.svg'),
+      image: ('./src/assets/tsgg-pc.svg'),
     },
   ]
 
-  return ( 
-    <div className="my-projects-home-container">
-      <h1>My Complete Projects</h1>
-      <ProjectCard />
-      <div>
-        <img src="./src/assets/wellfit-pc.svg" alt="wellfit app project card" />
-        <h3>WellFit - Workout Tracker</h3>
-        <p>Python / Django / Docker</p>
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0
+    } else if (newIndex >= items.length) {
+      newIndex = items.length - 1
+    }
+    setActiveIndex(newIndex)
+  }
+
+  return (
+      <div className="carousel">
+        <div 
+          className="inner" 
+          style={{ transform: `translate(-${activeIndex * 100}%)` }}
+        >
+          {items.map((item) => {
+            return <ProjectCard item={item} key={item.name} width="100%" />
+            }
+          )}
+        </div>
+
+      <div className="carousel-btns">
+
+        <button
+          onClick={() => {
+            updateIndex(activeIndex - 1);
+          }}
+          className="arrow-btn"
+        >
+          &lt;
+        </button>
+
+        <div className="indicators">
+          {items.map((item, index) => {
+            return (
+              <button
+                onClick={() => {
+                  updateIndex(index);
+                }}
+                className='indicator-btn'
+                key={index}
+              >
+                .
+              </button>
+            )
+          })}
+
+        </div>
+        <button
+          onClick={() => {
+            updateIndex(activeIndex + 1);
+          }}
+          className="arrow-btn"
+        >
+          &gt;
+        </button>
+          </div>
       </div>
-      <div>
-        <img src="./src/assets/wandr-pc.svg" alt="wellfit app project card" />
-        <h3>Wandr - Travel Themed Social Media</h3>
-        <p>Python / Django / Docker</p>
-      </div>
-      <div>
-        <img src="./src/assets/yyh-pc.svg" alt="wellfit app project card" />
-        <h3>The Yum Yum Hub - Recipe Collector</h3>
-        <p>Python / Django / Docker</p>
-      </div>
-      <div>
-        <img src="./src/assets/word-whimsy-pc.svg" alt="wellfit app project card" />
-        <h3>Word Whimsy - Online Journaling</h3>
-        <p>JavaScript / Express.js / Docker</p>
-      </div>
-      <div>
-        <img src="./src/assets/tsgg-pc.svg" alt="wellfit app project card" />
-        <h3>The Silly Goose Grape-Venture - Choose Your Own Adventure Game</h3>
-        <p>Python / Django / Docker</p>
-      </div>
-    </div>
+
   )
 }
+
 
 export default ProjectList 
--- src/components/Home/ProjectCard.jsx
+++ src/components/Home/ProjectCard.jsx
@@ -1,7 +1,12 @@
-const ProjectCard = () => {
+import './Project.css'
+
+const ProjectCard = ({item}) => {
   return ( 
-    <>
-    <h1>Project Carousel Card</h1></>
+    <div className="carousel-item">
+      <img className="carousel-img" src={item.image} alt="image of app" />
+      <div className="carousel-item-text"><h3>{item.name}</h3></div>
+      <div className="carousel-item-text"><p>{item.tech}</p></div>
+    </div>
   )
 }
 
