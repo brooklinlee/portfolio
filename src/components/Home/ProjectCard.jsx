818e2242c4b975e585d5344a6ca65b7024cc8fba
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-14 21:11:15 -0700

    minor nav bar fix, prep app for deployment

--- src/components/Home/ProjectCard.jsx
+++ src/components/Home/ProjectCard.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+
 import './Project.css'
 
 const ProjectCard = ({item}) => {
   return ( 
     <div className="carousel-item">
       <img className="carousel-img" src={item.image} alt="image of app" />
-      <div className="carousel-item-text"><h3>{item.name}</h3></div>
+      <Link to='/projects'>
+        <div className="carousel-item-text"><h3>{item.name}</h3></div>
+      </Link>
       <div className="carousel-item-text"><p>{item.tech}</p></div>
     </div>
   )
