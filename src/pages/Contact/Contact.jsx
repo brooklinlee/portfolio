f3ee8599d88192290f0c94d2ddad627dd5a659dd
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-14 19:40:33 -0700

    add links to github and linkedin on contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,10 +8,12 @@ const Contact = () => {
   return ( 
     <>
     <h1>Let's Chat!</h1>
-    <h3>Say hello using the form below, or visit my LinkedIn and GitHub profiles by clicking the icons!</h3>
+    <h3>Say hello using the form below, or visit my LinkedIn and GitHub profiles by clicking the icons below!</h3>
     <ContactForm />
-    <a href=""><img src="./src/assets/6.svg" alt="GitHub Logo" /></a>
-    <a href=""><img src="./src/assets/7.svg" alt="LinkedIn Logo" /></a>
+    <div className="contact-links">
+      <a href="https://github.com/brooklinlee"><img src="./src/assets/6.svg" alt="GitHub Logo" /></a>
+      <a href="http://www.linkedin.com/in/brooklin-lee"><img src="./src/assets/7.svg" alt="LinkedIn Logo" /></a>
+    </div>
     </>
   )
 }
