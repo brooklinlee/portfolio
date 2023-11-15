5befd42fd9cd5b71f35eba79fa07f15ffa30367b
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-14 19:49:16 -0700

    add media query to contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,8 +7,8 @@ import './Contact.css'
 const Contact = () => {
   return ( 
     <>
-    <h1>Let's Chat!</h1>
-    <h3>Say hello using the form below, or visit my LinkedIn and GitHub profiles by clicking the icons below!</h3>
+    <h1 className="lets-chat">Let's Chat!</h1>
+    <h3 className="say-hello">Say hello using the form below, or visit my LinkedIn and GitHub profiles by clicking the icons below!</h3>
     <ContactForm />
     <div className="contact-links">
       <a href="https://github.com/brooklinlee"><img src="./src/assets/6.svg" alt="GitHub Logo" /></a>
