6e3638590505caa8de9b93669faff23f0d09210d
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-17 14:05:00 -0700

    move linkedin and github links above contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -13,11 +13,11 @@ const Contact = () => {
     <>
     <h1 className="lets-chat">Let's Chat!</h1>
     <h3 className="say-hello">Say hello using the form below, or visit my LinkedIn and GitHub profiles by clicking the icons below!</h3>
-    <ContactForm />
     <div className="contact-links">
       <a href="https://github.com/brooklinlee"><img src={github} alt="GitHub Logo" /></a>
       <a href="http://www.linkedin.com/in/brooklin-lee"><img src={linkedIn} alt="LinkedIn Logo" /></a>
     </div>
+    <ContactForm />
     </>
   )
 }
